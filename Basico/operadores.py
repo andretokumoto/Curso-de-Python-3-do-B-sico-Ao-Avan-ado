8017445b13585d9ddca20589268d682c1f915840
andretokumoto <andretokumoto@gmail.com>
2024-06-27 13:48:31 -0300

    operadores lógicos

--- Basico/operadores.py
+++ Basico/operadores.py
@@ -3,4 +3,12 @@
 # 3. * / // %
 # 4. + -
 conta = (1 + int(0.5 + 0.5)) ** (5 + 5)
-print(conta)
+print('resultado= '+ str(conta))
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
