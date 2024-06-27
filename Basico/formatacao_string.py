754cb4ff156b2fe2e2f5e03d1630adfc20abc9d0
andretokumoto <andretokumoto@gmail.com>
2024-06-27 14:06:04 -0300

    interpolação,f-string e fatiamento de strings

--- Basico/formatacao_string.py
+++ Basico/formatacao_string.py
@@ -8,3 +8,35 @@ formato = 'nome = {}  imc = {}'.format(nome,imc)
 
 print(mensagem)
 print(formato)
+
+#interpolação de string
+
+nome = 'André'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %08X' % (1500, 1500))
+
+#f-string
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{1000.4873648123746:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
+print(f'{variavel!r}')
+
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[::-1])
+
