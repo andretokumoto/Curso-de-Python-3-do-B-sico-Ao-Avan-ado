7f65c6fef00af659b5197d59f111419aec2dc868
andretokumoto <andretokumoto@gmail.com>
2024-07-03 15:57:51 -0300

    parametros não definidos na função

---
+++ intermediário (seção 4)/args.py
@@ -0,0 +1,11 @@
+x,y,*resto  = 1,2,3,4,5,6
+print(x,y,resto)
+
+def soma(*args):
+    resultado = 0 
+    for numero in args:
+        resultado +=numero
+        print(numero)
+    return resultado
+
+print(soma(1,2,3,4,5,6))
