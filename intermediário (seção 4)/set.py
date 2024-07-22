c964a7a7c59ae460c3655f4bff7ab2a03f3789bc
andretokumoto <andretokumoto@gmail.com>
2024-07-22 11:39:39 -0300

    metodos de set

--- intermediário (seção 4)/set.py
+++ intermediário (seção 4)/set.py
@@ -7,3 +7,14 @@ print(s1,type(s1))
 
 #sets removem valores duplicados
 
+#metodos add, update, clear e discard
+
+s2 = set()
+s2.add('Tokumoto') #so´adiciona um valor por vez
+print(s2)
+s2.update(('andre',1,2,3))#pode dser usado para mandar varios valores
+print(s2)
+s2.discard('Tokumoto')#elimina o valor passado
+print(s2)
+s2.clear()#limpa o set
+print(s2)
