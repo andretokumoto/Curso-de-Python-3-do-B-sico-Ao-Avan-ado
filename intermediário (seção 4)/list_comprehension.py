b85886c2d5873b953167ccaf1873474435fc11e5
andretokumoto <andretokumoto@gmail.com>
2024-07-22 13:46:12 -0300

    List comprehension

---
+++ intermediário (seção 4)/list_comprehension.py
@@ -0,0 +1,8 @@
+
+# cria um for e atribui valores na lista em sua declaração
+lista  =[numero for numero in range(10)]
+print(lista)
+
+#pode ser aplica uma logica
+lista  =[numero*10 for numero in range(10)]
+print(lista)
