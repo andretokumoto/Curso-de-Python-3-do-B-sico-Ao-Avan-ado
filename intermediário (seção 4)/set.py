1ad53a040a41a874ed597a51be14d56f54b9e74b
andretokumoto <andretokumoto@gmail.com>
2024-07-22 11:49:09 -0300

    conjuntos em sets

--- intermediário (seção 4)/set.py
+++ intermediário (seção 4)/set.py
@@ -18,3 +18,20 @@ s2.discard('Tokumoto')#elimina o valor passado
 print(s2)
 s2.clear()#limpa o set
 print(s2)
+
+#conjuntos em sets
+
+#union
+s2.update(('Palmeiras',1914,1999,1951,2020,2021,'andre'))
+s3 = s1 | s2
+print(s3)
+
+#intersecção
+s3 = s1 & s2
+print(s3)
+
+#diferenças
+s3 = s2 - s1
+print(s3)
+s3 = s1 ^ s2 #diferença simetrica, apenas itens fora da intersecção
+print(s3)
