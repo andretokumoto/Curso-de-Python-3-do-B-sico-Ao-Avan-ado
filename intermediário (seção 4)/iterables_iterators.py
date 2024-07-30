3795d49ba7f0f03c7620cf0e9d962d055d7b78d0
andretokumoto <andretokumoto@gmail.com>
2024-07-30 13:54:15 -0300

    generator

--- intermediário (seção 4)/iterables_iterators.py
+++ intermediário (seção 4)/iterables_iterators.py
@@ -1,3 +1,5 @@
+import sys
+
 iterable = ['eu','tenho','_iter_']
 
 #iterator = iterable.__iter__()
@@ -6,4 +8,17 @@ iterator = iter(iterable)
 print(next(iterator))
 print(next(iterator))
 print(next(iterator))
+print('-----')
 
+#generator
+
+lista = [n for n in range(10000)]
+generator = (n for n in range(100)) #definida no uso do parenteses -> ( <- n for n in range(100) -> ) <-
+
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
+print('------')
+
+
+for i in range(50):
+    print(next(generator))
