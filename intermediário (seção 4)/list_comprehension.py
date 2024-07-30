4b298f5be549277777a5b59433c2eb7e926a3192
andretokumoto <andretokumoto@gmail.com>
2024-07-30 12:03:01 -0300

    map list comprehension

--- intermediário (seção 4)/list_comprehension.py
+++ intermediário (seção 4)/list_comprehension.py
@@ -6,3 +6,17 @@ print(lista)
 #pode ser aplica uma logica
 lista  =[numero*10 for numero in range(10)]
 print(lista)
+print("")
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 30, },
+]
+novos_produtos = [
+    {**produto, 'preco' : produto['preco']*0.9}
+    for produto in produtos
+]
+
+print (*novos_produtos, sep='\n')
