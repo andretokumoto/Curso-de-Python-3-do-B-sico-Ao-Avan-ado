c5a2ffa999a249b74baabfc70aedafc0627a0b4b
andretokumoto <andretokumoto@gmail.com>
2024-12-13 15:08:32 -0300

    with open

--- intermediário (seção 4)/arquivos.py
+++ intermediário (seção 4)/arquivos.py
@@ -1,6 +1,20 @@
 caminho = 'exemplo_arquivo.txt'
 
+'''
 arquivo = open(caminho,'w')
 arquivo.write('texto do arquivo')
 arquivo.close()
+'''
 
+with open(caminho,'w') as arquivo:
+    arquivo.write('Texto com with open\n')
+    arquivo.writelines(
+        ('linha 1\n','linha 2\n')
+    )
+ 
+with open(caminho,'r') as arquivo:   
+    arquivo.seek(0, 0)
+
+    print('READLINES')
+    for linha in arquivo.readlines():
+        print(linha.strip())
