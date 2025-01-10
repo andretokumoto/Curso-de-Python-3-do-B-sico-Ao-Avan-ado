195ec123f85b3160bc7171ec14b716fad8fe92b2
andretokumoto <andretokumoto@gmail.com>
2025-01-10 13:47:16 -0300

    classes, instancias e metodos em instancias

---
+++ Programação orientada a objetos (seção 5)/classes_instancias.py
@@ -0,0 +1,16 @@
+﻿class Pessoa():
+    
+    def __init__(self,nome,sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        
+    def estudar(self):
+        print(f'{self.nome} {self.sobrenome} está estudando\n')
+        
+p1 = Pessoa("Andre","Filipe")
+print(p1.nome, p1.sobrenome)
+print()
+p1.estudar()
+
+p2 = Pessoa('Geraldino','Macarena')
+p2.estudar()
